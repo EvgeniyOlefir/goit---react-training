ea20e6b5b45474422ba73c1ba899db29db379ff4
Evgeniy Olefir <evgeniy.olefir@gmail.com>
2021-03-16 20:17:26 +0200

    Добавил маршрутизацию

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,18 +1,32 @@
-import { routes } from "../routes";
-import styles from "./content.module.scss";
+// import { routes } from '../routes';
+import { Route } from 'react-router-dom';
+import styles from './content.module.scss';
+import UsersPage from '../pages/UsersPage';
+import HomePage from '../pages/HomePage';
+// import HomeWorksPage from '../pages/HomeWorksPage';
+// import AboutPage from '../pages/AboutPage';
+import BooksPage from '../pages/BooksPage';
+// import CountersPage from '../pages/CountersPage';
+import UserDetailsPage from '../pages/UserDetailsPage';
+import TodosPage from '../pages/TodosPage';
 
 const Content = () => {
-  const { pathname } = window.location;
+  // const { pathname } = window.location;
   return (
     <div className={styles.content}>
-      {routes.map(
+      {/* {routes.map(
         ({ path, component: Component }) => pathname === path && <Component key={path} />
-      )}
+      )} */}
       {/* {pathname === "/" && <HomePage />}
       {pathname === "/users-page" && <UsersPage />}
       {pathname === "/home-works" && <HomeWorksPage />}
       {pathname === "/about" && <AboutPage />}
       {pathname === "/books" && <BooksPage />} */}
+      <Route path="/" exact component={HomePage} />
+      <Route path="/users-page" component={UsersPage} />
+      <Route path="/user-details" component={UserDetailsPage} />
+      <Route path="/books" component={BooksPage} />
+      <Route path="/todos" component={TodosPage} />
     </div>
   );
 };
--- src/routes.js
+++ src/routes.js
@@ -1,51 +1,51 @@
-import HomePage from "./pages/HomePage";
-import UsersPage from "./pages/UsersPage";
-import HomeWorksPage from "./pages/HomeWorksPage";
-import AboutPage from "./pages/AboutPage";
-import BooksPage from "./pages/BooksPage";
-import CountersPage from "./pages/CountersPage";
-import UserDetailsPage from "./pages/UserDetailsPage";
-import TodosPage from "./pages/TodosPage";
+import HomePage from './pages/HomePage';
+import UsersPage from './pages/UsersPage';
+// import HomeWorksPage from './pages/HomeWorksPage';
+// import AboutPage from './pages/AboutPage';
+import BooksPage from './pages/BooksPage';
+import CountersPage from './pages/CountersPage';
+import UserDetailsPage from './pages/UserDetailsPage';
+import TodosPage from './pages/TodosPage';
 
 export const routes = [
   {
-    path: "/",
-    label: "Home",
+    path: '/',
+    label: 'Home',
     component: HomePage,
   },
   {
-    path: "/users-page",
-    label: "UsersPage",
+    path: '/users-page',
+    label: 'UsersPage',
     component: UsersPage,
   },
+  // {
+  //   path: "/home-works",
+  //   label: "HomeWorksPage",
+  //   component: HomeWorksPage,
+  // },
+  // {
+  //   path: "/about",
+  //   label: "AboutPage",
+  //   component: AboutPage,
+  // },
   {
-    path: "/home-works",
-    label: "HomeWorksPage",
-    component: HomeWorksPage,
-  },
-  {
-    path: "/about",
-    label: "AboutPage",
-    component: AboutPage,
-  },
-  {
-    path: "/user-details",
-    label: "User Details",
+    path: '/user-details',
+    label: 'User Details',
     component: UserDetailsPage,
   },
   {
-    path: "/counters",
-    label: "Counters",
+    path: '/counters',
+    label: 'Counters',
     component: CountersPage,
   },
   {
-    path: "/books",
-    label: "BooksPage",
+    path: '/books',
+    label: 'BooksPage',
     component: BooksPage,
   },
   {
-    path: "/todos",
-    label: "Todos",
+    path: '/todos',
+    label: 'Todos',
     component: TodosPage,
   },
 ];
