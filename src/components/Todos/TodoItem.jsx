ccab5a1a11c630aea8db275b0e33e11f3ff0c435
Evgeniy Olefir <evgeniy.olefir@gmail.com>
2021-03-16 14:18:36 +0200

    Добавил стилизацию todo

--- src/components/Todos/TodoItem.jsx
+++ src/components/Todos/TodoItem.jsx
@@ -1,4 +1,15 @@
 import moment from 'moment';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+
+const useStyles = makeStyles({
+  todosItem: {
+    marginTop: 10,
+    backgroundColor: '#eeeeff',
+  },
+});
 
 const TodosItem = ({
   todo: { id, value, urgency, date, isDone },
@@ -8,15 +19,20 @@ const TodosItem = ({
   const formattedDate = moment(date).format('YYYY/MM/DD hh:mm:ss');
   const handleDelete = () => onDelete(id);
   const handleToggle = () => onToggle(id);
+  const classes = useStyles();
 
   return (
-    <div>
-      <p>{value}</p>
-      <p>{urgency}</p>
-      <p>{formattedDate}</p>
-      <input type="checkbox" checked={isDone} onChange={handleToggle} />
-      <button onClick={handleDelete}>x</button>
-    </div>
+    <Card className={classes.todosItem}>
+      <CardContent>
+        <p>{value}</p>
+        <p>{urgency}</p>
+        <p>{formattedDate}</p>
+      </CardContent>
+      <CardActions>
+        <input type="checkbox" checked={isDone} onChange={handleToggle} />
+        <button onClick={handleDelete}>x</button>
+      </CardActions>
+    </Card>
   );
 };
 
--- src/components/Todos/TodosInputForm.jsx
+++ src/components/Todos/TodosInputForm.jsx
@@ -1,15 +1,16 @@
-import { useState } from "react";
-import s from "../Todos/todosInputForm.module.scss";
+import { useState } from 'react';
+import s from '../Todos/todosInputForm.module.scss';
+import Button from '@material-ui/core/Button';
 
 const TodosInputForm = ({ onSubmit }) => {
   const [showForm, setShowForm] = useState(false);
-  const [value, setValue] = useState("");
-  const [urgency, setUrgency] = useState("low");
+  const [value, setValue] = useState('');
+  const [urgency, setUrgency] = useState('low');
 
-  const toggleForm = () => setShowForm((prevValue) => !prevValue);
-  const handleInputChange = (e) => setValue(e.target.value);
-  const handleUrgencyChange = (e) => setUrgency(e.target.value);
-  const handleSubmit = (e) => {
+  const toggleForm = () => setShowForm(prevValue => !prevValue);
+  const handleInputChange = e => setValue(e.target.value);
+  const handleUrgencyChange = e => setUrgency(e.target.value);
+  const handleSubmit = e => {
     e.preventDefault();
 
     if (!value) return;
@@ -23,8 +24,8 @@ const TodosInputForm = ({ onSubmit }) => {
     };
 
     onSubmit(newTodo);
-    setValue("");
-    setUrgency("low");
+    setValue('');
+    setUrgency('low');
     setShowForm(false);
   };
 
@@ -39,7 +40,7 @@ const TodosInputForm = ({ onSubmit }) => {
               <input
                 type="radio"
                 value="low"
-                checked={urgency === "low"}
+                checked={urgency === 'low'}
                 onChange={handleUrgencyChange}
               />
               <span>low</span>
@@ -49,7 +50,7 @@ const TodosInputForm = ({ onSubmit }) => {
               <input
                 type="radio"
                 value="medium"
-                checked={urgency === "medium"}
+                checked={urgency === 'medium'}
                 onChange={handleUrgencyChange}
               />
               <span>medium</span>
@@ -59,7 +60,7 @@ const TodosInputForm = ({ onSubmit }) => {
               <input
                 type="radio"
                 value="high"
-                checked={urgency === "high"}
+                checked={urgency === 'high'}
                 onChange={handleUrgencyChange}
               />
               <span>high</span>
@@ -71,7 +72,10 @@ const TodosInputForm = ({ onSubmit }) => {
           </div>
         </form>
       ) : (
-        <button onClick={toggleForm}>+ Add todo</button>
+        // <button onClick={toggleForm}>+ Add todo</button>
+        <Button variant="contained" color="primary" onClick={toggleForm}>
+          + Add todo
+        </Button>
       )}
     </div>
   );
